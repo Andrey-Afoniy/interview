4bddf0ae86d0aafa7f3851f783ec88f47cd5e299
Andrey-Afoniy <mr.knop2015@yandex.ru>
2026-03-06 16:30:11 +0400

    interview

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,World");
+        List<String> names = List.of("Андрей", "Дима");
+        List<String> result = processNames(names);
+        System.out.println(result);
+    }
+
+    public static List<String> processNames(List<String> names) {
+        if (names == null) return List.of();
+
+        return names.stream()
+                .filter(name -> name != null && !name.isEmpty())
+                .map(name -> name.substring(1))
+                .collect(Collectors.toList());
     }
 }
